78bef71948901de7a1a5ac062e9c03f7cf4f2023
gitongaisaac <gitongagisaac@gmail.com>
2023-04-09 12:06:56 +0300

    made changes

--- script.js
+++ script.js
@@ -3,17 +3,38 @@ const body = document.querySelector("body");
 const dropDash = document.querySelector(".drop-dashes");
 const dropDown = document.querySelector(".dropdown");
 const dropDownCard = document.querySelector(".dropdown.card li");
+const links = document.querySelectorAll(".dropdown li");
+// console.log(links);
 
 dropDash.addEventListener("click", (e) => {
   const target = e.target.parentNode;
-  console.log(target);
   dropDown.classList.toggle("hide");
+});
 
-  // mouseleave
-  document.addEventListener("click", (e) => {
-    if (e.target !== dropDash) {
-      console.log("The target is not the drop down");
-      // dropDown.classList.add("hide");
+document.addEventListener("click", (e) => {
+  console.log(e.target.classList);
+  if (dropDown.classList.contains("hide")) {
+    console.log("The dropdown is hidden");
+  } else {
+    if (
+      e.target.classList.contains("item") ||
+      e.target.classList.contains("item-link") ||
+      e.target.classList.contains("dropdown")
+    ) {
+      dropDown.classList.add("hide");
+    } else {
+      dropDown.classList.remove("hide");
     }
-  });
+  }
+  // if (
+  //   e.target.classList.contains("item") ||
+  //   e.target.classList.contains("item-link") ||
+  //   e.target.classList.contains("dropdown")
+  // ) {
+  //   console.log("The dropdown");
+  //   dropDown.classList.add("hide");
+  // } else {
+  //   console.log("Not the dropdown");
+  //   dropDown.classList.remove("hide");
+  // }
 });
