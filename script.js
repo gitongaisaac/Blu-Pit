234cc3393e57267ac871ccd2697976066f73b0a4
gitongaisaac <gitongagisaac@gmail.com>
2023-06-07 16:17:59 +0300

    a commit

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const dropDash = document.querySelector(".drop-dashes");
 const dropDown = document.querySelector(".dropdown");
 const dropDownCard = document.querySelector(".dropdown.card li");
 const links = document.querySelectorAll(".dropdown li");
-// console.log(links);
+const login = document.querySelector(".login");
+const signUp = document.querySelector(".sign-up-form");
+const loginForm = document.querySelector(".login-form");
+const sign_up = document.querySelector(".sign-up");
+const signIn = document.querySelector(".sign-in");
+const logIn = document.querySelector(".log-in");
 
 dropDash.addEventListener("click", (e) => {
   const target = e.target.parentNode;
@@ -12,9 +17,8 @@ dropDash.addEventListener("click", (e) => {
 });
 
 document.addEventListener("click", (e) => {
-  console.log(e.target.classList);
   if (dropDown.classList.contains("hide")) {
-    console.log("The dropdown is hidden");
+    // console.log("The dropdown is hidden");
   } else {
     if (
       e.target.classList.contains("item") ||
@@ -26,15 +30,30 @@ document.addEventListener("click", (e) => {
       dropDown.classList.remove("hide");
     }
   }
-  // if (
-  //   e.target.classList.contains("item") ||
-  //   e.target.classList.contains("item-link") ||
-  //   e.target.classList.contains("dropdown")
-  // ) {
-  //   console.log("The dropdown");
-  //   dropDown.classList.add("hide");
-  // } else {
-  //   console.log("Not the dropdown");
-  //   dropDown.classList.remove("hide");
+
+  // if (!login.classList.contains("hide")) {
+  //   login.classList.add("hide");
   // }
+});
+
+login.addEventListener("click", (e) => {
+  if (e.target.classList.contains("sign-up")) {
+    signUp.classList.remove("hide");
+    loginForm.classList.add("hide");
+    sign_up.style.borderBottom = "2px solid var(--primary-500)";
+    signIn.style.borderBottom = "none";
+  } else if (e.target.classList.contains("sign-in")) {
+    signUp.classList.add("hide");
+    loginForm.classList.remove("hide");
+    signIn.style.borderBottom = "2px solid var(--primary-500)";
+    sign_up.style.borderBottom = "none";
+  }
+
+  if (e.target.classList.contains("close")) {
+    login.classList.add("hide");
+  }
+});
+
+logIn.addEventListener("click", (e) => {
+  login.classList.toggle("hide");
 });
